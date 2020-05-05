c56ec5def826e3c2c154d3adca50283b673b11ef
avovana <vladimir.englishbox@gmail.com>
2020-05-05 13:22:46 +0300

    [feat] Ansible

---
+++ ansible/install_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# example
+## ./install_environment.sh inventory/localhost.yml
+
+main() {
+      ansible_config=$1
+      printf "Ansible config: $ansible_config\n"
+      time ansible-playbook 02_deploy_source.yml -i $ansible_config
+      echo $(date)
+}
+
+main ${@}
+
